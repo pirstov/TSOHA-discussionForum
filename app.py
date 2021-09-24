c23ba3bf25d3f32934e37f2b2ccb7d2a40ea9d74
Ville Pirsto <ville.pirsto@gmail.com>
2021-09-24 12:31:51 +0300

    Added account registration and DB table for users

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+# Flask-related libraries
+from logging import debug
 from flask import Flask
 from flask import redirect, render_template, request, session
+
+# For hashing the passwords
+from werkzeug.security import check_password_hash, generate_password_hash
 
 # For generating a secret key
 import secrets
@@ -17,6 +22,27 @@ app.secret_key = SECRET_KEY
 @app.route("/")
 def index():
 	return render_template("index.html")
+
+# User registration page
+@app.route("/register")
+def register():
+	pass
+
+# Account creation process
+@app.route("/createaccount", methods=["POST"])
+def createaccount():
+	username = request.form["username"]
+	# generate the hash for the password directly
+	password = generate_password_hash(request.form["password"])
+
+	# ----------------------------------
+	# TBD: implement functionality to check if the username is already in use
+	# ----------------------------------
+
+	# Insert the new user into the appropriate database table
+	sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+	db.session.execute(sql, {"username":username, "pasword":password})
+	db.session.commit()
 
 # Log in page
 @app.route("/loginpage")
--- schema.sql
+++ schema.sql
@@ -1,1 +1,5 @@
 /* This file includes the database schema */
+
+
+/* Table for storing users */
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
