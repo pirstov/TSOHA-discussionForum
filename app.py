9796656fae0dc66046cde3a4e25d6ac264f1dd00
Ville Pirsto <ville.pirsto@gmail.com>
2021-10-04 18:47:32 +0300

    Changed order of threads into descending by posting time

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ def section(id):
 	# TBD: Implement counting of messages within each thread
 	# -----------------------------------------------
 	sql = "SELECT T.id, T.posting_time, T.thread_name, S.section_name, U.username FROM sections S LEFT JOIN threads T"\
-	" ON T.section_id = S.id  LEFT JOIN users U ON T.user_id = U.id WHERE section_id=:id"
+	" ON T.section_id = S.id  LEFT JOIN users U ON T.user_id = U.id WHERE section_id=:id ORDER BY T.posting_time DESC"
 	result = db.session.execute(sql, {"id": id})
 	threads = result.fetchall()
 	return render_template("section.html", id = id, threads = threads)
